6d5a2fa2264deadc898f27548863496dfee76409
Sahil Kumar <kumarsahil8981@gmail.com>
2023-04-24 11:55:34 +0530

    footer done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Section/Navbar';
+import Footer from './Section/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Footer/>
     </div>
   );
 }
