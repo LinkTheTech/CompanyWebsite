eebef590e4edf7cd78fd378dc076351a8728263c
Anupam kumar <100180990+hlv-kakashi@users.noreply.github.com>
2023-04-29 12:04:49 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './Pages/Homepage';
+import ActionAreaCard from './Component/ActionAreaCard';
+import OurServices from './Section/OurServices';
 
 function App() {
   return (
     <div className="App">
-      
+      <Homepage></Homepage>
+      <OurServices></OurServices>
     </div>
   );
 }
--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import AccordionFAQ from '../Section/AccordionFAQ'
 
 const Homepage = () => {
   return (
+    <>
     <div>Homepage</div>
+    <AccordionFAQ></AccordionFAQ>
+    </>
+
   )
 }
 
